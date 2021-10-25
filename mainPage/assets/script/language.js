219923d152f7c04df6fed48314a1927ecacb1c68
Chylox <chylox_bwin@protonmail.com>
2021-10-25 21:41:09 +0200

    Connect Buttons to js getting data from json

--- mainPage/assets/script/language.js
+++ mainPage/assets/script/language.js
@@ -1,3 +1,15 @@
+// Connection to JSON Data
 let englishData = JSON.parse(language);
+// console.table(englishData[1].aboutMeHeading);
 
-console.table(englishData);
+// Variable Declaration:
+const engButton = document.querySelector('#englishButton');
+const gerButton = document.querySelector('#germanButton');
+
+engButton.addEventListener('click', () => {
+    console.table(englishData[1].aboutMeHeading);
+})
+
+gerButton.addEventListener('click', () => {
+    console.table(englishData[0].aboutMeHeading);
+})
